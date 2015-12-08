9087686af97625fd61b2ca80a885e651074c057a
info@davidpaulrosser.co.uk <info@davidpaulrosser.co.uk>
2015-12-08 10:47:17 +0000

    fixed color aspect

--- frag.glsl
+++ frag.glsl
@@ -11,14 +11,14 @@ void main() {
 	vec2 uv = gl_FragCoord.xy/iResolution.xy;
 	float aspect = iResolution.x / iResolution.y;
 
-	uv.x *= aspect;
-
 	vec3 color = vec3(uv.x,uv.y,0.0);
+
+	uv.x *= aspect;
 
 	vec2 mouse = vec2(iMouse.xy);
 	mouse.x *= aspect;
 
-	float radius = map(sin(iGlobalTime), -1.0, 1.0, 0.25, 0.5);
+	float radius = map(sin(iGlobalTime), -1.0, 1.0, 0.25, 0.3);
 
 	if(distance(uv.xy, mouse) < radius){
 	 	color.x = 2.0 - color.x;

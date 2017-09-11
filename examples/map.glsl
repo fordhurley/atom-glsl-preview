24164c52f4611370de635dd0a289d8272446b613
Ford Hurley <ford.hurley@gmail.com>
2017-09-11 08:37:50 -0400

    Basic glslify integration (no error handling)

---
+++ examples/map.glsl
@@ -0,0 +1,5 @@
+float map(float value, float inMin, float inMax, float outMin, float outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+#pragma glslify: export(map)
